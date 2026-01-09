1a2f0da91a70e6dc4fd212a7c5606c1783f2781a
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2026-01-09 19:32:46 +1000

    feat: praetorian neruo & new smoke effects

---
+++ Content.Shared/_MC/Xeno/Projectiles/MCXenoProjectileInjectComponent.cs
@@ -0,0 +1,14 @@
+﻿using Content.Shared.Chemistry.Reagent;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._MC.Xeno.Projectiles;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class MCXenoProjectileInjectComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public string Solution = "chemicals";
+
+    [DataField, AutoNetworkedField]
+    public List<ReagentQuantity> Reagents = new();
+}
---
+++ Content.Shared/_MC/Xeno/Projectiles/MCXenoProjectileInjectSystem.cs
@@ -0,0 +1,31 @@
+﻿using Content.Shared._MC.Armor;
+using Content.Shared.Chemistry.EntitySystems;
+using Content.Shared.Inventory;
+using Content.Shared.Projectiles;
+
+namespace Content.Shared._MC.Xeno.Projectiles;
+
+public sealed class MCXenoProjectileInjectSystem : EntitySystem
+{
+    [Dependency] private readonly SharedSolutionContainerSystem _solution = null!;
+    [Dependency] private readonly MCArmorSystem _mcArmor = null!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<MCXenoProjectileInjectComponent, ProjectileHitEvent>(OnHit);
+    }
+
+    private void OnHit(Entity<MCXenoProjectileInjectComponent> entity, ref ProjectileHitEvent args)
+    {
+        if (!_solution.TryGetSolution(args.Target, entity.Comp.Solution, out var solution, out _))
+            return;
+
+        var armor = MCArmorSystem.ArmorToValue(_mcArmor.GetArmor(args.Target, SlotFlags.HEAD)?.Bio ?? 0);
+        foreach (var reagentQuantity in entity.Comp.Reagents)
+        {
+            _solution.TryAddReagent(solution.Value, reagentQuantity.Reagent, reagentQuantity.Quantity * armor, out _);
+        }
+    }
+}
