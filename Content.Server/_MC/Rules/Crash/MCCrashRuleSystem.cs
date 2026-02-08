8d08dd434465134ef970638f1a8aa9162882f5cf
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2026-02-09 07:20:36 +1000

    feat: ftl now

--- Content.Server/_MC/Rules/Crash/MCCrashRuleSystem.cs
+++ Content.Server/_MC/Rules/Crash/MCCrashRuleSystem.cs
@@ -1,10 +1,9 @@
-﻿﻿using Content.Server._MC.Xeno;
+﻿using Content.Server._MC.Xeno;
 using Content.Server._MC.Xeno.Hive;
 using Content.Server._MC.Xeno.Spawn;
 using Content.Server._RMC14.Power;
 using Content.Server.GameTicking;
 using Content.Server.Maps;
-using Content.Server.Preferences.Managers;
 using Content.Server.RoundEnd;
 using Content.Server.Shuttles.Components;
 using Content.Server.Shuttles.Systems;
