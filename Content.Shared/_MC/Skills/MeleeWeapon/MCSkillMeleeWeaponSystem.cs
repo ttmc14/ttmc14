3a6cc1473d15c94b685d87ad6d0ca9d71487cc38
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2026-02-17 19:30:06 +1000

    feat: melee weapons skill

---
+++ Content.Shared/_MC/Skills/MeleeWeapon/MCSkillMeleeWeaponSystem.cs
@@ -0,0 +1,36 @@
+﻿using Content.Shared._RMC14.Marines.Skills;
+using Content.Shared.Weapons.Melee.Events;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._MC.Skills.MeleeWeapon;
+
+public sealed class MCSkillMeleeWeaponSystem : EntitySystem
+{
+    private static readonly EntProtoId<SkillDefinitionComponent> SkillMeleeWeapons = "MCSkillMeleeWeapons";
+    private static readonly EntProtoId<SkillDefinitionComponent> SkillCqc = "MCSkillCqc";
+
+    private static readonly float SkillMeleeWeaponBuff = 0.15f;
+    private static readonly float SkillCqcBuff = 0f;
+
+    [Dependency] private readonly SkillsSystem _rmcSkills = null!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<GetMeleeDamageEvent>(OnGetMeleeDamage);
+        SubscribeLocalEvent<GetMeleeAttackRateEvent>(OnGetMeleeAttackRate);
+    }
+
+    private void OnGetMeleeDamage(ref GetMeleeDamageEvent ev)
+    {
+        var skill = _rmcSkills.GetSkill(ev.User, SkillMeleeWeapons);
+        ev.Damage += ev.Damage * SkillMeleeWeaponBuff * skill;
+    }
+
+    private void OnGetMeleeAttackRate(GetMeleeAttackRateEvent ev)
+    {
+        var skill = _rmcSkills.GetSkill(ev.User, SkillCqc);
+        ev.Rate += SkillCqcBuff * skill;
+    }
+}
