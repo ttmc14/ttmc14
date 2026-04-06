59541de0e5958cd8c6784c4eda8e46a34bd21fe0
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2026-04-06 11:19:24 +1000

    fix: neuro

--- Content.Shared/_MC/Mob/Stamina/MCStaminaSystem.API.cs
+++ Content.Shared/_MC/Mob/Stamina/MCStaminaSystem.API.cs
@@ -4,7 +4,7 @@ namespace Content.Shared._MC.Mob.Stamina;
 
 public sealed partial class MCStaminaSystem
 {
-    public void ApplyDamage(Entity<MCStaminaComponent?> entity, float amount, bool updateTimer = true)
+    public void ApplyDamage(Entity<MCStaminaComponent?> entity, float amount, bool updateTimer = true, bool forceTimer = false)
     {
         if (!Resolve(entity, ref entity.Comp))
             return;
@@ -18,11 +18,19 @@ public sealed partial class MCStaminaSystem
 
         entity.Comp.Stamina = float.Clamp(newStamina, 0, entity.Comp.StaminaMax);
 
-        if (amount > 0 && updateTimer)
-            entity.Comp.NextRegenTime = _timing.CurTime + entity.Comp.RegenDelay;
+        if (amount > 0 && updateTimer || forceTimer)
+            UpdateTimer(entity);
 
         CheckExhaustion((entity.Owner, entity.Comp));
         UpdateStaminaAlert((entity.Owner, entity.Comp));
+    }
+
+    public void UpdateTimer(Entity<MCStaminaComponent?> entity)
+    {
+        if (!Resolve(entity, ref entity.Comp))
+            return;
+
+        entity.Comp.NextRegenTime = _timing.CurTime + entity.Comp.RegenDelay;
     }
 
     public float GetLoss(Entity<MCStaminaComponent?> entity)
