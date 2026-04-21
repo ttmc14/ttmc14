b212497381f9f221d568f0e967c244774e10cf25
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2026-04-21 23:59:26 +1000

    feat: AI barricade destroying

---
+++ Content.Server/_MC/AI/Sensors/MCAISensorNearestBarricade.cs
@@ -0,0 +1,11 @@
+﻿using Content.Shared._MC.AI.Modules;
+using Content.Shared._RMC14.Entrenching;
+
+namespace Content.Server._MC.AI.Sensors;
+
+public sealed partial class MCAISensorNearestBarricade : MCAISensorNearestComponent<MCAISensorNearestBarricade>
+{
+    public override TimeSpan UpdateInterval => TimeSpan.FromSeconds(0.5f);
+}
+
+public sealed partial class MCAISensorNearestBarricadeSystem : MCAISensorNearestWithComponentSystem<MCAISensorNearestBarricade, BarricadeComponent>;
