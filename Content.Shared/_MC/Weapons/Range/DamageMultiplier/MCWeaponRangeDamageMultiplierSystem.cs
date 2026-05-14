3f8c8ac6e32958116377fbff13d5a446abde415d
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2026-05-15 03:38:13 +1000

    feat: r500 & r312

---
+++ Content.Shared/_MC/Weapons/Range/DamageMultiplier/MCWeaponRangeDamageMultiplierSystem.cs
@@ -0,0 +1,25 @@
+﻿using Content.Shared.Projectiles;
+using Content.Shared.Weapons.Ranged.Events;
+
+namespace Content.Shared._MC.Weapons.Range.DamageMultiplier;
+
+public sealed class MCWeaponRangeDamageMultiplierSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<MCWeaponRangeDamageMultiplierComponent, AmmoShotEvent>(OnAmmoShot);
+    }
+
+    private void OnAmmoShot(Entity<MCWeaponRangeDamageMultiplierComponent> weapon, ref AmmoShotEvent args)
+    {
+        for (var t = args.FiredProjectiles.Count - 1; t >= 0; --t)
+        {
+            var uid = args.FiredProjectiles[t];
+            if (!TryComp<ProjectileComponent>(args.FiredProjectiles[t], out var projectileComponent))
+                continue;
+
+            projectileComponent.Damage *= weapon.Comp.Multiplier;
+            Dirty(uid, projectileComponent);
+        }
+    }
+}
