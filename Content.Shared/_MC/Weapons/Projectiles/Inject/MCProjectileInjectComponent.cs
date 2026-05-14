c17ec0bdf9560db2601966e8250d6713887b7afc
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2026-05-14 23:52:52 +1000

    feat: albedo

---
+++ Content.Shared/_MC/Weapons/Projectiles/Inject/MCProjectileInjectComponent.cs
@@ -0,0 +1,27 @@
+﻿using Content.Shared.Chemistry.Reagent;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._MC.Weapons.Projectiles.Inject;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class MCProjectileInjectComponent : Component
+{
+    [DataField]
+    public string Solution = "chemicals";
+
+    [DataField]
+    public List<ReagentQuantity> Reagents = new();
+
+    [DataField]
+    public bool IgnoreArmor;
+
+    #region Effects
+
+    [DataField]
+    public bool Effect = true;
+
+    [DataField]
+    public Color EffectColor = Color.DarkOrange;
+
+    #endregion
+}
