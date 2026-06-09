0de69fd16d92386d905f74a78927c86492fbe202
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2026-06-09 23:37:36 +1000

    feat: xeno phero tower

---
+++ Content.Shared/_MC/Xeno/Structures/PheromoneTowers/UI/MCXenoStructurePheromoneTowerSelectUI.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared._MC.Xeno.Constructions.PheromoneTowers.UI;
+
+[Serializable, NetSerializable]
+public enum MCXenoStructurePheromoneTowerSelectUI : byte
+{
+    Key,
+}
